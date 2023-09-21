a4e5c497ce90ee695afa779ecf93d549c6a62128
Code Sandbox <hello@codesandbox.io>
2023-09-21 18:18:49 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.dca5d58aa773370242aec874bf8fcff1.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bf5ddb8cf5d240d5efaade9baafaf1b",
+    "url": "https://deepaksm24.github.io/csb-z8ptvs/index.html"
+  },
+  {
+    "revision": "3e936cfc57a89966cdb7",
+    "url": "https://deepaksm24.github.io/csb-z8ptvs/static/css/main.bcc80384.chunk.css"
+  },
+  {
+    "revision": "e0dfe7dcd1b6b4cbe5ea",
+    "url": "https://deepaksm24.github.io/csb-z8ptvs/static/js/2.bbfcb347.chunk.js"
+  },
+  {
+    "revision": "cbc41762b08bce4213514217ce9f16f9",
+    "url": "https://deepaksm24.github.io/csb-z8ptvs/static/js/2.bbfcb347.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e936cfc57a89966cdb7",
+    "url": "https://deepaksm24.github.io/csb-z8ptvs/static/js/main.cde768e7.chunk.js"
+  },
+  {
+    "revision": "8501ca15ca1dc9e89f76",
+    "url": "https://deepaksm24.github.io/csb-z8ptvs/static/js/runtime-main.760bf8ec.js"
+  },
+  {
+    "revision": "2c685e9d2e4cd51841b855216a68a154",
+    "url": "https://deepaksm24.github.io/csb-z8ptvs/static/media/ChatKharaMeal-Website.2c685e9d.png"
+  },
+  {
+    "revision": "e0d4f3837450e809ff376dac878318d6",
+    "url": "https://deepaksm24.github.io/csb-z8ptvs/static/media/Expense-TrackerApp.e0d4f383.png"
+  },
+  {
+    "revision": "efc84b727ddd6a549bb13375cd45d615",
+    "url": "https://deepaksm24.github.io/csb-z8ptvs/static/media/Music-List-App.efc84b72.png"
+  },
+  {
+    "revision": "6429306807ffbd6e3b9a95f3a0a59786",
+    "url": "https://deepaksm24.github.io/csb-z8ptvs/static/media/QuizzApp-JavaScript.64293068.png"
+  },
+  {
+    "revision": "bf989778565c49da91f9e0ef13dbdc5c",
+    "url": "https://deepaksm24.github.io/csb-z8ptvs/static/media/background.bf989778.webp"
+  },
+  {
+    "revision": "e95d05630894e5403e569fdaa0a2b61a",
+    "url": "https://deepaksm24.github.io/csb-z8ptvs/static/media/background.e95d0563.webp"
+  },
+  {
+    "revision": "ffded2381d25dd906406d6e07f91dea5",
+    "url": "https://deepaksm24.github.io/csb-z8ptvs/static/media/bootstrap-4.ffded238.svg"
+  },
+  {
+    "revision": "3707da3c6a394c7ee2523273f8d98a25",
+    "url": "https://deepaksm24.github.io/csb-z8ptvs/static/media/css3.3707da3c.svg"
+  },
+  {
+    "revision": "10b0719343408095f5a9b369e0787115",
+    "url": "https://deepaksm24.github.io/csb-z8ptvs/static/media/firebase-icon.10b07193.svg"
+  },
+  {
+    "revision": "1190a19ce3cc940741a60137a1e074cb",
+    "url": "https://deepaksm24.github.io/csb-z8ptvs/static/media/git-icon.1190a19c.svg"
+  },
+  {
+    "revision": "567f006b9ca974f343db1c9c79920074",
+    "url": "https://deepaksm24.github.io/csb-z8ptvs/static/media/github.567f006b.svg"
+  },
+  {
+    "revision": "b38ff61aa327fbdc24f7b1dad7d04d1a",
+    "url": "https://deepaksm24.github.io/csb-z8ptvs/static/media/heroku.b38ff61a.svg"
+  },
+  {
+    "revision": "676e8188f003ed5d4882a0d0f378dad3",
+    "url": "https://deepaksm24.github.io/csb-z8ptvs/static/media/html-5.676e8188.svg"
+  },
+  {
+    "revision": "2d2e31cc63a96c7e6044f52d30fd21b9",
+    "url": "https://deepaksm24.github.io/csb-z8ptvs/static/media/javascript.2d2e31cc.svg"
+  },
+  {
+    "revision": "38f9a4f29ee819486c821b193f592aed",
+    "url": "https://deepaksm24.github.io/csb-z8ptvs/static/media/logo-vercel.38f9a4f2.svg"
+  },
+  {
+    "revision": "82291019f34ab54a677c456a84127cc3",
+    "url": "https://deepaksm24.github.io/csb-z8ptvs/static/media/material-ui-1.82291019.svg"
+  },
+  {
+    "revision": "ecb5ec57f305689a6dc44a0828e4b893",
+    "url": "https://deepaksm24.github.io/csb-z8ptvs/static/media/netlify-icon.ecb5ec57.svg"
+  },
+  {
+    "revision": "d8fee7924d463334d60c5bf965237552",
+    "url": "https://deepaksm24.github.io/csb-z8ptvs/static/media/profile.d8fee792.svg"
+  },
+  {
+    "revision": "83fe61b05935f090b671b08aeb77b63d",
+    "url": "https://deepaksm24.github.io/csb-z8ptvs/static/media/react-bootstrap.83fe61b0.svg"
+  },
+  {
+    "revision": "4009eef3b3e291f296e71962c70895c5",
+    "url": "https://deepaksm24.github.io/csb-z8ptvs/static/media/react.4009eef3.svg"
+  },
+  {
+    "revision": "aa0a337603611296524a666b62a9ff23",
+    "url": "https://deepaksm24.github.io/csb-z8ptvs/static/media/redux.aa0a3376.svg"
+  },
+  {
+    "revision": "b446ca5a397c1ef6fb94cd02c4e94335",
+    "url": "https://deepaksm24.github.io/csb-z8ptvs/static/media/sass-1.b446ca5a.svg"
+  },
+  {
+    "revision": "8bce7aef886c3872c218e0e6ef8bdccb",
+    "url": "https://deepaksm24.github.io/csb-z8ptvs/static/media/slide1.8bce7aef.webp"
+  },
+  {
+    "revision": "7256f309520276be70acd97720610bc6",
+    "url": "https://deepaksm24.github.io/csb-z8ptvs/static/media/slide2.7256f309.webp"
+  },
+  {
+    "revision": "8e5b87b60151049a8df9ec0bd30978f0",
+    "url": "https://deepaksm24.github.io/csb-z8ptvs/static/media/slide3.8e5b87b6.webp"
+  },
+  {
+    "revision": "760a3ee424f40cd7b8da50ac4818592f",
+    "url": "https://deepaksm24.github.io/csb-z8ptvs/static/media/tailwindcss-icon.760a3ee4.svg"
+  }
+]);
